9d4309ceedd299bacdab79a4d6ffc71d6ba47537
“imaditya12” <imadityarathore@gmail.com>
2023-11-17 01:56:14 +0530

    added changes

--- src/main/java/com/urlshortner/urlshortner/UrlshortnerApplication.java
+++ src/main/java/com/urlshortner/urlshortner/UrlshortnerApplication.java
@@ -2,12 +2,26 @@ package com.urlshortner.urlshortner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @SpringBootApplication
-public class UrlshortnerApplication {
+//@EnableWebMvc
+public class UrlshortnerApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(UrlshortnerApplication.class, args);
 	}
+	
+	
+//	@Override
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//	    registry.addResourceHandler("swagger-ui.html")
+//	      .addResourceLocations("classpath:/META-INF/resources/");
+//
+//	    registry.addResourceHandler("/webjars/**")
+//	      .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//	}
+
 
 }
